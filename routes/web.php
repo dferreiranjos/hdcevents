1155397988aeb4002d8cbad79d10e51c9e6def95
dferreiranjos <dferreiranjos@yahoo.com.br>
2021-09-07 21:42:21 -0300

    controller and migration

--- routes/web.php
+++ routes/web.php
@@ -1,35 +1,8 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\EventController;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
 
-Route::get('/', function () {
-
-    $nome = 'Daniel';
-    return view('welcome', ['nome'=>$nome]);
-});
-
-Route::get('/contact', function () {
-    return view('contact');
-});
-
-Route::get('/produtos', function () {
-
-    $busca = request('search');
-
-    return view('products', ['busca'=>$busca]);
-});
-
-Route::get('/produtos_teste/{id?}', function ($id = null) {
-    return view('product', ['id'=>$id]);
-});
+Route::get('/', [EventController::class, 'index']);
+Route::get('/events/create', [EventController::class, 'create']);
